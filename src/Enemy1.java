7853e28d4ec3be58cac4e4a208a7fd2c435bf97c
Allen <allenli873@gmail.com>
2017-04-30 22:27:27 -0700

    Frustration

--- src/Enemy1.java
+++ src/Enemy1.java
@@ -1,18 +1,24 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Enemy1 {
 	private final int PPM = 150;
-	private int x, y;
+	protected final int HEIGHT = PPM / 3;
+	protected final int WIDTH = PPM / 2;
+	protected int x, y;
 	private int[] xVals;
 	private int[] yVals;
 	public Enemy1(int x, int y) {
 		this.x = x;
-		this.y = y;
+		this.y = y - PPM / 3;
 	}
-	
+	//draws 
 	public void draw(Graphics g) {
 		x -= 1;
 		xVals = new int[]{x, x + PPM / 4, x + PPM / 2};
-		yVals = new int[]{};
+		yVals = new int[]{y + HEIGHT, y, y + HEIGHT};
+		Color c = new Color(139, 69, 19);
+		g.setColor(c);
+		g.fillPolygon(xVals, yVals, 3);
 	}
 }
--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.io.File;
 import java.io.IOException;
 
@@ -15,12 +16,14 @@ public class Game extends JPanel {
 	private PhysicsDash app;
 	
 	private Character player;
+	private Enemy1 enemy1;
 	private Image ground;
 	
 	public Game(PhysicsDash p) {
 		app = p;
 		setSize(960, 540);
 		player = new Character(app);
+		enemy1 = new Enemy1(500, 350);
 		loadGround();
 		addKeyListener(player);
 	}
@@ -39,13 +42,17 @@ public class Game extends JPanel {
 		
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.translate((int)  (app.WIDTH - player.w)/2 - player.x, 0);
-//		System.out.println("hi") ;
 		g.setColor(new Color(175, 60, 0));
-		//g.fillRect(0, PhysicsDash.HEIGHT - GROUND_HEIGHT, PhysicsDash.WIDTH, GROUND_HEIGHT);
-		//g.drawImage(ground, 0, PhysicsDash.HEIGHT - GROUND_HEIGHT, PhysicsDash.WIDTH, GROUND_HEIGHT, null);
 		for(int x = -3000; x < app.WIDTH + 3000; x += GROUND_HEIGHT) {
 			g.drawImage(ground, x, app.HEIGHT - GROUND_HEIGHT, GROUND_HEIGHT, GROUND_HEIGHT, null);
 		}
+		Rectangle r = new Rectangle((int)player.x, (int)player.y, player.w, player.h);
+		g.drawRect((int)player.x, (int)player.y, player.w, player.h);
+		Rectangle r2 = new Rectangle(enemy1.x, enemy1.y, enemy1.WIDTH, enemy1.HEIGHT);
+		g.drawRect(enemy1.x, enemy1.y, enemy1.WIDTH, enemy1.HEIGHT);
+		if(g2d.hit(r, r2, false))
+			System.exit(0);
 		player.draw(g);
+		enemy1.draw(g);
 	}
 }
