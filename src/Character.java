7ec9b045921b097a42ff5cab11aae588750eb29e
Allen <allenli873@gmail.com>
2017-04-30 21:56:26 -0700

    Extra Char stuff

--- src/Character.java
+++ src/Character.java
@@ -4,10 +4,11 @@ import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+//the character that is controlled
 public class Character implements KeyListener {
 	
 	private final float PPM = 150; //ppm is how many pixels represent one meter
-	
+	//field variables
 	private PhysicsDash app;
 	private Image character, ground;
 	private boolean jumped = false;
@@ -18,7 +19,7 @@ public class Character implements KeyListener {
 	float step = 1/60f;
 	float velY = 0;
 	float grav = 9.8f;
-	
+	//constructor
 	public Character(PhysicsDash p) {
 		app = p;
 		app.getMyImage();
@@ -28,10 +29,10 @@ public class Character implements KeyListener {
 		x = 0;
 		y = 0;
 	}
-	
+	//draws the character in the ever changing X/Y positions
 	public void draw(Graphics g) {
 		g.setColor(Color.RED);
-		
+		//checks if right or left was pressed
 		if(right)
 			x += 5;
 		if(left)
@@ -42,13 +43,11 @@ public class Character implements KeyListener {
 		if(y > app.HEIGHT - Game.GROUND_HEIGHT - h) {
 			y = app.HEIGHT - Game.GROUND_HEIGHT - h;
 			jumped = false;
-//			System.out.println(jumped);
 		}
 	}
-	
+	//makes sure character only jumps one time, jump by pressing w
 	public void keyTyped(KeyEvent e) {
 		char c = e.getKeyChar();
-//		System.out.println(c);
 		if(c == 'w') {
 			if(!jumped) {
 				velY = -4;
@@ -57,14 +56,17 @@ public class Character implements KeyListener {
 		}
 		
 	}
+	//move left or right by pressing and holding a and d respectively
 	public void keyPressed(KeyEvent e) {
-		System.out.println("called");
 		int code = e.getKeyCode();
+		//code for a
 		if(code == KeyEvent.VK_A)
 			left = true;
+		//code for d
 		if(code == KeyEvent.VK_D)
 			right = true;
 	}
+	//stops moving when key is released
 	public void keyReleased(KeyEvent e) {
 		int code = e.getKeyCode();
 		if(code == KeyEvent.VK_A)
